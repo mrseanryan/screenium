c2766c77fcadfb5fdbf78ebc54af115ac000d2bf
str <str@localhost>
2015-06-19 08:51:27 +0200

    initial commit of console app

---
+++ screenium/screenium/BrowserDriver.cs
@@ -0,0 +1,33 @@
+﻿
+namespace screenium
+{
+	class BrowserDriver
+	{
+
+		/*
+		IWebDriver driver = new ChromeDriver();
+
+		driver.Navigate().GoToUrl("http://www.google.com/");
+		
+		// Find the text input element by its name
+		IWebElement query = driver.FindElement(By.Name("q"));
+
+		// Enter something to search for
+		query.SendKeys("Cheese");
+
+		// Now submit the form. WebDriver will find the form for us from the element
+		query.Submit();
+
+		// Google's search is rendered dynamically with JavaScript.
+		// Wait for the page to load, timeout after 10 seconds
+		WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+		wait.Until((d) => { return d.Title.ToLower().StartsWith("cheese"); });
+
+		// Should see: "Cheese - Google Search"
+		System.Console.WriteLine("Page title is: " + driver.Title);
+
+		//Close the browser
+		driver.Quit();
+		 */
+	}
+}
