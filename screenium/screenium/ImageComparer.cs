0ee662ca0d379f0eccad7cad5a611f88d7a7bcd9
str <str@localhost>
2015-10-21 14:10:41 +0200

    adding code TODO about image comparison

--- screenium/screenium/ImageComparer.cs
+++ screenium/screenium/ImageComparer.cs
@@ -10,6 +10,24 @@ namespace screenium
     {
         internal CompareResult CompareImages(string actualImageFilePath, string expectedImageFilePath, string testName)
         {
+/* comparing:
+ImageComparer.Compare Method(Image, Image, Image)
+https://msdn.microsoft.com/en-us/library/hh135398.aspx
+*/
+
+/*
+blurring:
+https://msdn.microsoft.com/en-us/library/system.windows.media.effects.blureffect.aspx
+*/
+			
+		/* colorizing
+Namespace:   Microsoft.VisualStudio.TestTools.UITesting
+Assembly:  Microsoft.VisualStudio.TestTools.UITesting (in Microsoft.VisualStudio.TestTools.UITesting.dll)
+
+grayscale + colorizing:
+http://www.codeproject.com/Articles/3772/ColorMatrix-Basics-Simple-Image-Color-Adjustment
+*/
+
             throw new NotImplementedException();
         }
     }
