299ad34e6a237e02852e44f9f6912b7eca14f1d3
str <str@localhost>
2015-10-22 18:20:35 +0200

    adding TODO about HTML report to show diffs

--- screenium/screenium/ReportCreatorFactory.cs
+++ screenium/screenium/ReportCreatorFactory.cs
@@ -8,6 +8,10 @@ namespace screenium
     {
         internal static IReportCreator Create()
         {
+            //TODO make a HTML report creator (and run both of them)
+            //it can be just 1 html page, with the diff image for each test run.
+            //we can open it via explorer (ref WeeWebWatcher ?)
+
             return new ConsoleReportCreator();
         }
     }
